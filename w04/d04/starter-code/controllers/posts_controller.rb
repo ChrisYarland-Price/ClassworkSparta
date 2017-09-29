584e421a37363598d308c27af8a91c4efc6426f0
Chris Yarland-Price <chris.yarland@protonmail.com>
2017-09-29 15:12:30 +0100

    D05 done

--- w04/d04/starter-code/controllers/posts_controller.rb
+++ w04/d04/starter-code/controllers/posts_controller.rb
@@ -1,57 +1,88 @@
 class PostsController < Sinatra::Base
 
   #set the root of the app
-  set :root, File.join(File.dirname(_File_), '..')
+  set :root, File.join(File.dirname(__FILE__), '..')
 
   #set views directory
   set :views, Proc.new { File.join(root, "views") }
-  
+
 
   configure :development do
       register Sinatra::Reloader
   end
 
+  $posts = [{
+   id: 0,
+   title: "Post 1",
+   body: "This is the first post"
+  },
+  {
+    id: 1,
+    title: "Post 2",
+    body: "This is the second post"
+  },
+  {
+    id: 2,
+    title: "Post 3",
+    body: "This is the third post"
+  }]
+
   get '/' do
-  
-  "<h1>Homepage</h1>"
-  
+    @title = 'Blog Posts'
+    @posts = $posts
+    erb :'posts/index'  
   end
 
   get '/new'  do
     
-    "NEW"
+    erb :'posts/new'
     
   end
     
   get '/:id' do
     
-    id = params[:id]
-    
-    "SHOW: #{id}"
-    
+    id = params[:id].to_i
+    # @title = $posts[id][:title]
+    # @body = $posts[id][:body]
+    @post = $posts.select{ |post| post[:id] == id}
+    erb :'posts/show'    
   end
     
   post '/' do
-    
-    "CREATE"
-    
+    new_post = {
+      id: $posts.length,
+      title: params[:title],
+      body: params[:body]
+    }
+    $posts.push new_post
+    redirect '/'    
   end
     
   put '/:id'  do
-    
-    "UPDATE: #{params[:id]}"
-    
+    id = params[:id].to_i
+    # get the post out of the database
+    post = $posts[id]
+    # update values
+    post[:title] = params[:title]
+    post[:body] = params[:body]
+    # save the post back into the data store
+    $posts[id] = post
+
+
+    redirect '/'
   end
     
   delete '/:id'  do
-    
-    "DELETE: #{params[:id]}"
+    id = params[:id].to_i
+    $posts.select! { |post| post[:id] != id  }
+    redirect '/'
     
   end
     
   get '/:id/edit'  do
-    
-    "EDIT: #{params[:id]}"
+    id = params[:id].to_i
+    @post = $posts[id]
+    erb :'posts/edit'
     
   end
 
--- w04/d05/models-sinatra/config.ru
+++ w04/d05/models-sinatra/config.ru
@@ -1,6 +1,7 @@
 require 'sinatra'
+require 'pg'
 require 'sinatra/reloader' if development?
-require 'erb'
+require_relative './models/post.rb'
 require_relative './controllers/posts_controller.rb'
 
 use Rack::Reloader
